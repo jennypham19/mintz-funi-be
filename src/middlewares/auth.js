72425476d05f7f559132c5aaa9175af7876b1d41
mintzdg1 <locpuntr@gmail.com>
2025-06-28 17:31:12 +0700

    fix middleware

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -5,31 +5,44 @@ const { User } = require('../../models');
 const ApiError = require('../utils/ApiError');
 const catchAsync = require('../utils/catchAsync');
 
-const protect = catchAsync(async (req, res, next) => {
-  let token;
-  if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
-    token = req.headers.authorization.split(' ')[1];
-  } else if (req.cookies.token) {
-    token = req.cookies.token;
+const protect = async (req, res, next) => {
+  try {
+    let token;
+    if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+      token = req.headers.authorization.split(' ')[1];
+    } else if (req.cookies.token) {
+      token = req.cookies.token;
+    }
+
+    if (!token) {
+      return next(new ApiError(StatusCodes.UNAUTHORIZED, 'Bạn chưa đăng nhập.'));
+    }
+
+    let decoded;
+    try {
+      decoded = jwt.verify(token, config.jwt.secret);
+    } catch (error) {
+      if (error instanceof jwt.TokenExpiredError) {
+        return next(new ApiError(StatusCodes.UNAUTHORIZED, 'Access token đã hết hạn.'));
+      }
+      return next(new ApiError(StatusCodes.UNAUTHORIZED, 'Access token không hợp lệ.'));
+    }
+
+    const currentUser = await User.findByPk(decoded.sub, {
+      attributes: { exclude: ['password'] },
+    });
+
+    if (!currentUser) {
+      return next(new ApiError(StatusCodes.UNAUTHORIZED, 'Người dùng của token này không còn tồn tại.'));
+    }
+
+    req.user = currentUser;
+    next();
+
+  } catch (error) {
+    return next(error);
   }
-
-  if (!token) {
-    return next(new ApiError(StatusCodes.UNAUTHORIZED, 'Bạn chưa đăng nhập, vui lòng đăng nhập để tiếp tục.'));
-  }
-
-  const decoded = jwt.verify(token, config.jwt.secret);
-
-  const currentUser = await User.findByPk(decoded.sub, {
-    attributes: { exclude: ['password'] },
-  });
-
-  if (!currentUser) {
-    return next(new ApiError(StatusCodes.UNAUTHORIZED, 'Người dùng của token này không còn tồn tại.'));
-  }
-
-  req.user = currentUser;
-  next();
-});
+};
 
 const authorize = (...roles) => {
   return (req, res, next) => {
