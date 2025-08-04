2c7c9728b7bee18c02e74fcad713cd1dacbebd5f
anhpn <ngocanh.pham19042001@gmail.com>
2025-08-04 17:08:47 +0700

    change_3

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -99,7 +99,8 @@ const logout = async (refreshToken) => {
   if (!refreshTokenDoc) {
     throw new ApiError(StatusCodes.NOT_FOUND, 'Refresh token không tồn tại');
   }
-  await tokenService.blacklistRefreshToken(refreshToken)
+  await refreshTokenDoc.destroy();
+  // await tokenService.blacklistRefreshToken(refreshToken)
 };
 
 const changePassword = async (updateBody) => {
